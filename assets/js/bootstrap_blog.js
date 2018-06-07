3aa0dc61af48a984a985a87148f276806303b35e
Ayoze Hernández Díaz <ayoze.dev@gmail.com>
2018-06-07 10:10:16 +0100

    Removed unnecessary files and comments

--- assets/js/bootstrap_blog.js
+++ assets/js/bootstrap_blog.js
@@ -1,13 +1,5 @@
 (function() {
   $(document).ready(function() {
-    // Bootstrap utilities init
     $('[data-toggle="tooltip"]').tooltip();
-    //($('.collapse').collapse();
   });
 })(jQuery)
-
-/**
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
- */

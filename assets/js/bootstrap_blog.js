712e562f6fa90de1a90e777cc050a94878bd5ee3
Ayoze Hernández Díaz <ayoze.dev@gmail.com>
2018-07-12 15:41:36 +0100

    Added go to top button

--- assets/js/bootstrap_blog.js
+++ assets/js/bootstrap_blog.js
@@ -1,5 +1,20 @@
 (function() {
   $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+      });
   });
 })(jQuery)
